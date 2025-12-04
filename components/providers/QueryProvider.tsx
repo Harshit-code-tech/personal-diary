f4a12ab4819650b927b6ed5b6af0ef2bc86c2ab1
Harshit-code-tech <harshitghosh7@gmail.com>
2025-12-04 17:50:59 +0530

    typo fix

--- components/providers/QueryProvider.tsx
+++ components/providers/QueryProvider.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client"
 
-import { QueryClientProvider } from '@tantml:function_calls>
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import { useEffect } from 'react'
 import { setupGlobalErrorHandler } from '@/lib/error-logger'
